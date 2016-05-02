b9e01d09bdc5a1b3bd5874cf1afd43b0cce91daf
Raymond Booth-154002924 <rmbooth92084@gmail.com>
2016-05-02 14:08:57 -0700

    PowerUPModles RB

---
+++ Assets/ItemS.js
@@ -0,0 +1,21 @@
+﻿#pragma strict
+var up : boolean = true;		 
+
+function Start () {
+
+}
+
+function Update () {
+	if(transform.position.y >=245)
+		up = false;
+	if(transform.position.y <=243)
+		up = true;
+
+	if(up)
+ 		transform.position.y +=0.04;
+	else
+		transform.position.y -=0.04;
+
+	transform.Rotate(Vector3.up * 3);
+
+}
