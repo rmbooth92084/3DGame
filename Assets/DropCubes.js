fe7741bc9d55c6f9d5c5afec2137a6c5dbd1ee81
Christopher Butler-373000409 <373000409@vusdnoc.vusd.k12.ca.us>
2016-05-10 13:44:56 -0700

    yolo

---
+++ Assets/DropCubes.js
@@ -0,0 +1,23 @@
+﻿#pragma strict
+var box:GameObject;
+
+function Start () {
+
+}
+
+function Update () {
+DropCube(transform.position);
+}
+function DropCube(pos:Vector3)
+{
+	yield WaitForSeconds(2.0);
+	Instantiate(box, pos, Quaternion.identity);
+}
+function OnTriggerEnter(other:Collider)
+{
+
+if(other.tag=="box")
+print("feggit");
+		Destroy(gameObject);
+	
+}
